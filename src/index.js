2da539996b37825229c0e02637873567e97ee7eb
Kent C. Dodds <me+github@kentcdodds.com>
2020-08-19 19:15:17 -0600

    Reduce max time to 5 minutes

--- src/index.js
+++ src/index.js
@@ -867,7 +867,7 @@ Goodbye 👋
 }
 
 async function cleanup(guild) {
-  const maxWaitingTime = 1000 * 60 * 30
+  const maxWaitingTime = 1000 * 60 * 5
   const tooManyMessages = 100
   const timeoutWarningMessageContent = `it's been a while and I haven't heard from you. This channel will get automatically deleted and you'll be removed from the server after a while. Don't worry though, you can always try again later when you have time to finish: https://kcd.im/discord`
   const spamWarningMessageContent = `you're sending a lot of messages, this channel will get deleted automatically if you send too many.`

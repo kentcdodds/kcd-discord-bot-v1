f54bbfcb56f23e7d8f3caf1415805429244ad518
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-11 23:01:18 -0600

    add kifs

---
+++ src/commands/command-fns/kif.js
@@ -0,0 +1,34 @@
+// Command purpose:
+// this command is just to make sure the bot is running
+const {getArgs} = require('../command-regex')
+
+const kifs = {
+  sweeet: 'https://giphy.com/gifs/sweet-flip-roller-blades-MDxjbPCg6DGf8JclbR',
+  thanks: 'https://giphy.com/gifs/thank-you-kentcdodds-hV7Vz9VwUaedWJZIxp',
+  agreed: 'https://giphy.com/gifs/agreed-kentcdodds-IcudIFZssABGjEeceT',
+  agree: 'https://giphy.com/gifs/agreed-kentcdodds-IcudIFZssABGjEeceT',
+  'mind blown':
+    'https://giphy.com/gifs/mind-blown-amazed-kentcdodds-fYB7BTqxTvBIvNeS5l',
+  bravo: 'https://giphy.com/gifs/applause-kentcdodds-hU8sgywxcUkYad9O6D',
+  applause: 'https://giphy.com/gifs/applause-kentcdodds-hU8sgywxcUkYad9O6D',
+  "you're awesome":
+    'https://giphy.com/gifs/youre-awesome-kentcdodds-SXlgWqXyqddHfGCc8n',
+  'aw, thanks':
+    'https://giphy.com/gifs/thanks-aw-kentcdodds-W6QJfAyl7hFezUcXvd',
+  spinning: 'https://giphy.com/gifs/space-react-kentcdodds-S6fwymvLr3hfFCEIU9',
+  rar: 'https://giphy.com/gifs/jumping-react-kentcdodds-iHt6BBLEFNqS2HWoi7',
+  excited: 'https://giphy.com/gifs/excited-kentcdodds-cjPz6B53w0VvI0Oi4G',
+}
+
+function kif(message) {
+  const args = getArgs(message.content)
+  if (kifs[args]) {
+    return message.channel.send(kifs[args])
+  }
+  return message.channel.send(`Couldn't find a keme for: "${args}"`)
+}
+kif.description = 'Send a KCD gif'
+kif.help = message =>
+  message.channel.send(`Available kifs are: ${kifs.join(', ')}`)
+
+module.exports = kif

3a5e649075c842a390871644b53081fb2247664a
Jacob Evans <cloud887@gmail.com>
2020-09-12 17:28:30 -1000

    delete last in collection find of additional message of identical origins and content within 10secs

--- src/admin/deduping-channel-posts.js
+++ src/admin/deduping-channel-posts.js
@@ -1,9 +1,8 @@
 export function dedupeMessages(client) {
   client.on('message', message => {
-    // this function can check whether the content of the message you pass is the same as this message
     const getMessages = msg => {
       return (
-        msg.content.toLowerCase() == message.content.toLowerCase() &&
+        msg.content.toLowerCase() === message.content.toLowerCase() &&
         msg.author === message.author
       )
     }
@@ -13,10 +12,7 @@ export function dedupeMessages(client) {
         maxMatches: 1,
         time: 10 * 1000,
       })
-      .then(collected => {
-        console.log(collected)
-        // this function will be called when a message matches you filter
-      })
+      .then(collected => collected.last().delete())
       .catch(console.error)
   })
 }

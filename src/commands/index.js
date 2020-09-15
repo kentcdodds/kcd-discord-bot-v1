be1d50e06addd3a3cd040abae191f9ec6ad6c461
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-15 12:37:37 -0600

    allow help to be shown when sending "help"

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,11 +2,19 @@ const {regex: commandRegex} = require('./command-regex')
 const commands = require('./commands')
 
 function handleNewMessage(message) {
-  const {command} = message.content.match(commandRegex)?.groups ?? {}
+  const {command, args} = message.content.match(commandRegex)?.groups ?? {}
   if (!command) return
 
   const commandFn = commands[command]
   if (!commandFn) return
+
+  if (args.toLowerCase().trim() === 'help') {
+    if (commandFn.help) {
+      return commandFn.help(message)
+    } else if (commandFn.description) {
+      return message.channel.send(commandFn.description)
+    }
+  }
 
   return commandFn(message)
 }

fab041f2052ce7d975f0eae6c9ee1774ab05620f
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-15 11:18:53 -0600

    improve kif logic

--- src/commands/command-fns/kif.js
+++ src/commands/command-fns/kif.js
@@ -4,14 +4,12 @@ const leven = require('leven')
 const {default: matchSorter} = require('match-sorter')
 const {getArgs} = require('../command-regex')
 
-const kifs = {
+const kifsBase = {
   sweet: 'https://giphy.com/gifs/sweet-flip-roller-blades-MDxjbPCg6DGf8JclbR',
   thanks: 'https://giphy.com/gifs/thank-you-kentcdodds-hV7Vz9VwUaedWJZIxp',
-  agreed: 'https://giphy.com/gifs/agreed-kentcdodds-IcudIFZssABGjEeceT',
   agree: 'https://giphy.com/gifs/agreed-kentcdodds-IcudIFZssABGjEeceT',
   'mind blown':
     'https://giphy.com/gifs/mind-blown-amazed-kentcdodds-fYB7BTqxTvBIvNeS5l',
-  bravo: 'https://giphy.com/gifs/applause-kentcdodds-hU8sgywxcUkYad9O6D',
   applause: 'https://giphy.com/gifs/applause-kentcdodds-hU8sgywxcUkYad9O6D',
   "you're awesome":
     'https://giphy.com/gifs/youre-awesome-kentcdodds-SXlgWqXyqddHfGCc8n',
@@ -20,15 +18,11 @@ const kifs = {
   spinning: 'https://giphy.com/gifs/space-react-kentcdodds-S6fwymvLr3hfFCEIU9',
   rar: 'https://giphy.com/gifs/jumping-react-kentcdodds-iHt6BBLEFNqS2HWoi7',
   excited: 'https://giphy.com/gifs/excited-kentcdodds-cjPz6B53w0VvI0Oi4G',
-  beautiful: 'https://giphy.com/gifs/chefs-kiss-kentcdodds-JtH6Z1hYsrdnAWEdtZ',
   'chefs kiss':
     'https://giphy.com/gifs/chefs-kiss-kentcdodds-JtH6Z1hYsrdnAWEdtZ',
-  cute: 'https://giphy.com/gifs/awwwww-kentcdodds-H1qlGJ78yOaePRcUBy',
   adorable: 'https://giphy.com/gifs/awwwww-kentcdodds-H1qlGJ78yOaePRcUBy',
-  aw: 'https://giphy.com/gifs/awwwww-kentcdodds-H1qlGJ78yOaePRcUBy',
-  duh: 'https://giphy.com/gifs/oh-huh-duh-jOzQAhuT3WUp19JldV',
   oh: 'https://giphy.com/gifs/oh-huh-duh-jOzQAhuT3WUp19JldV',
-  hmm: 'https://giphy.com/gifs/hmmm-eyebrow-kentcdodds-J2yDtNrNFsNDBJk2rR',
+  hm: 'https://giphy.com/gifs/hmmm-eyebrow-kentcdodds-J2yDtNrNFsNDBJk2rR',
   gotcha: 'https://giphy.com/gifs/gotcha-kentcdodds-XeGeQIzpqfL7GtcTMH',
   heehee: 'https://giphy.com/gifs/heehee-kentcdodds-kFHbtKZjQgU7VN46N2',
   hi: 'https://giphy.com/gifs/hi-kentcdodds-VbzmrabLQFE8VbQY3V',
@@ -36,43 +30,46 @@ const kifs = {
   'laugh cry': 'https://giphy.com/gifs/laugh-cry-sob-XHw7m2zy93EAjBLTZS',
   'laugh huh': 'https://giphy.com/gifs/what-laughing-wait-W23NVmwsjQ1xaXVomI',
   no: 'https://giphy.com/gifs/nooooo-kentcdodds-VzfU9S4UsFW0PeISZJ',
-  noo: 'https://giphy.com/gifs/nooooo-kentcdodds-VzfU9S4UsFW0PeISZJ',
-  nooo: 'https://giphy.com/gifs/nooooo-kentcdodds-VzfU9S4UsFW0PeISZJ',
-  noooo: 'https://giphy.com/gifs/nooooo-kentcdodds-VzfU9S4UsFW0PeISZJ',
-  nooooo: 'https://giphy.com/gifs/nooooo-kentcdodds-VzfU9S4UsFW0PeISZJ',
-  noooooo: 'https://giphy.com/gifs/nooooo-kentcdodds-VzfU9S4UsFW0PeISZJ',
-  slide: 'https://giphy.com/gifs/sliding-in-kentcdodds-mBYW2Fl1euZQXppJWN',
-  peak: 'https://giphy.com/gifs/sliding-in-kentcdodds-mBYW2Fl1euZQXppJWN',
+  peek: 'https://giphy.com/gifs/sliding-in-kentcdodds-mBYW2Fl1euZQXppJWN',
   this: 'https://giphy.com/gifs/kentcdodds-Lq6JZYxWUFdabFfwGF',
   waiting: 'https://giphy.com/gifs/waiting-Y2yKmMembXGbQLb0ne',
-  hurry: 'https://giphy.com/gifs/waiting-Y2yKmMembXGbQLb0ne',
   yes: 'https://giphy.com/gifs/yes-kentcdodds-mBwNA1bBnbHcZqhaLI',
-  awesome: 'https://giphy.com/gifs/yes-kentcdodds-mBwNA1bBnbHcZqhaLI',
   peace: 'https://giphy.com/gifs/peace-kentcdodds-j1ygW1a3xnc7UNtj6g',
   'peace fall':
     'https://giphy.com/gifs/fall-peace-kentcdodds-U3nGECxxmHugNeAm6n',
 }
-const kifsEmojis = {
-  '🍬': 'sweet',
-  '🙏': 'thanks',
-  '🤯': 'mind blown',
-  '👏': 'applause',
-  '🤩': 'excited',
-  '🧑‍🍳': 'chefs kiss',
-  '🥺': 'adorable',
-  '😮': 'oh',
-  '😈': 'heehee',
-  '👋': 'hi',
-  '😕': 'laugh huh',
-  '🙅': 'no',
-  '⛰️': 'peak',
-  '☝️': 'this',
-  '🏃': 'hurry',
-  '👍': 'yes',
-  '☮️': 'peace',
+
+const aliases = {
+  agree: ['agreed'],
+  no: ['🙅', 'noo', 'nooo'],
+  sweet: ['🍬'],
+  thanks: ['🙏'],
+  'mind blown': ['🤯', 'wow'],
+  applause: ['👏', 'bravo'],
+  excited: ['🤩'],
+  'chefs kiss': ['🧑‍🍳', 'beautiful'],
+  adorable: ['🥺', 'cute', 'aw'],
+  oh: ['😮', 'duh'],
+  heehee: ['😈'],
+  hi: ['👋'],
+  'laugh huh': ['😕'],
+  peak: ['slide'],
+  this: ['☝️'],
+  hurry: ['🏃', 'waiting'],
+  hm: ['hmm', 'hmmm'],
+  yes: ['👍', 'awesome'],
+  peace: ['☮️', 'goodbye'],
 }
-const emojiRegEx = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/gim
-const kifKeys = Object.keys(kifs)
+
+const kifs = {...kifsBase}
+for (const kifAlias of Object.keys(aliases)) {
+  const base = kifsBase[kifAlias]
+  for (const alias of aliases[kifAlias]) {
+    kifs[alias] = base
+  }
+}
+
+const kifKeys = Object.keys(kifs).sort()
 
 function getCloseMatches(search) {
   return Array.from(
@@ -104,10 +101,6 @@ const listify = (array, joiner) =>
 
 function kif(message) {
   const args = getArgs(message.content)
-  if (args.match(emojiRegEx) && kifsEmojis[args]) {
-    // convert emoji into relevant key kif string then compute kif object
-    return message.channel.send(kifs[kifsEmojis[args]])
-  }
   if (kifs[args]) {
     return message.channel.send(kifs[args])
   }

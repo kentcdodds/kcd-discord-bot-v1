57835d42d8c6be9d51d70ef0c419ccf5a0ee5f3c
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-16 11:55:18 -0600

    fix tests for kifs to not use github

--- src/commands/command-fns/__tests__/kif.js
+++ src/commands/command-fns/__tests__/kif.js
@@ -1,4 +1,67 @@
+const {rest} = require('msw')
+const {setupServer} = require('msw/node')
 const kif = require('../kif')
+
+const server = setupServer(
+  rest.get(
+    'https://api.github.com/repos/kentcdodds/kifs/contents/kifs.json',
+    (req, res, ctx) => {
+      return res(
+        ctx.json({
+          encoding: 'base64',
+          content: Buffer.from(
+            JSON.stringify({
+              hi: {
+                gif: 'https://giphy.com/gifs/hi-kentcdodds-VbzmrabLQFE8VbQY3V',
+                emojiAliases: ['👋'],
+              },
+              'aw shucks': {
+                gif:
+                  'https://giphy.com/gifs/koala-bear-kody-kentcdodds-kC3NfswHHg0AXtsywr',
+                emojiAliases: ['🐨', '😊'],
+              },
+              sweet: {
+                gif:
+                  'https://giphy.com/gifs/sweet-flip-roller-blades-MDxjbPCg6DGf8JclbR',
+                emojiAliases: ['🍬', '🍭'],
+              },
+              thanks: {
+                gif:
+                  'https://giphy.com/gifs/thank-you-kentcdodds-hV7Vz9VwUaedWJZIxp',
+                emojiAliases: ['🙏'],
+              },
+              agree: {
+                gif:
+                  'https://giphy.com/gifs/agreed-kentcdodds-IcudIFZssABGjEeceT',
+                aliases: ['agreed'],
+              },
+              peace: {
+                gif:
+                  'https://giphy.com/gifs/peace-kentcdodds-j1ygW1a3xnc7UNtj6g',
+                aliases: ['goodbye'],
+                emojiAliases: ['☮️'],
+              },
+              'peace fall': {
+                gif:
+                  'https://giphy.com/gifs/fall-peace-kentcdodds-U3nGECxxmHugNeAm6n',
+              },
+              adorable: {
+                gif:
+                  'https://giphy.com/gifs/awwwww-kentcdodds-H1qlGJ78yOaePRcUBy',
+                aliases: ['cute', 'aw'],
+                emojiAliases: ['🥺'],
+              },
+            }),
+          ).toString('base64'),
+        }),
+      )
+    },
+  ),
+)
+
+beforeAll(() => server.listen({onUnhandledRequest: 'error'}))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test('sends a gif', async () => {
   const send = jest.fn(() => Promise.resolve())
@@ -41,7 +104,7 @@ test('suggests several items (but no more than 6)', async () => {
   expect(send.mock.calls[0][0]).toMatchInlineSnapshot(`
     "Couldn't find a kif for: \\"a\\"
 
-    Did you mean \\"aw\\", \\"adorable\\", \\"agree\\", \\"agreed\\", \\"applause\\", or \\"aw shucks\\"?"
+    Did you mean \\"aw\\", \\"adorable\\", \\"agree\\", \\"agreed\\", \\"aw shucks\\", or \\"peace\\"?"
   `)
   expect(send).toHaveBeenCalledTimes(1)
 })

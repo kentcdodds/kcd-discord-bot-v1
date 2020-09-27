1d18a825b20d00c04984fd6793a0b0b45bf829a2
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-26 19:56:01 -0600

    fix english

--- src/admin/exclusive-epic-react-rocket.js
+++ src/admin/exclusive-epic-react-rocket.js
@@ -10,7 +10,7 @@ async function handleGuildMemberUpdate(oldMember, newMember) {
     const botsChannel = getChannel(newMember.guild, {name: 'talk-to-bots'})
     await botsChannel.send(
       `
-Hi ${newMember.user}, I noticed you tried to add a rocket to your nickname. I'm afraid you can't do this because you're discord account is not connected to your EpicReact.Dev account. Go to <https://epicreact.dev/discord> to make that connection.
+Hi ${newMember.user}, I noticed you tried to add a rocket to your nickname. I'm afraid you can't do this because your discord account is not connected to your EpicReact.Dev account. Go to <https://epicreact.dev/discord> to make that connection.
 
 If you don't have an https://EpicReact.Dev account, you should check it out. It's pretty great 😉 🚀
       `.trim(),

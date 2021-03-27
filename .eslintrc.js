ff76a59f3271e74aadf2001cc0a1187006f17b8f
Kent C. Dodds <me+github@kentcdodds.com>
2021-03-27 09:42:47 -0600

    update all deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-base-to-string': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/sort-type-union-intersection-members': 'off',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'off',
   },
 }
